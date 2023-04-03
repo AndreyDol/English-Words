cb0810a7ec7444d30839a27a7c93ace22c167a22
AndreyDol <113788568+AndreyDol@users.noreply.github.com>
2023-04-03 15:27:52 +0300

    addd data  function

--- src/index.js
+++ src/index.js
@@ -7,23 +7,65 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 const DEBOUNCE_DELAY = 700;
 let translatedText;
-let word ="";
+let word = '';
+let data=[];
+const URL = 'https://64298cb1ebb1476fcc4bb610.mockapi.io';
+
 // Пошук поля вводу та місця для додавання списку в DOM
 const refs = {
-  name: document.querySelector('#search-box'),
+  engWord: document.querySelector('#search-box'),
   ruWord: document.querySelector('#ru-box'),
   countriesList: document.querySelector('.country-list'),
   countriesInfo: document.querySelector('.country-info'),
   translateText: document.querySelector('.translate-text'),
   buttonPlay: document.querySelector('.button-play'),
+  buttonAdd: document.querySelector('.button-add'),
+  buttonNext: document.querySelector('.button-next'),
+};
+//Приймає данні зсайту 
+const readData = () => {
+  return fetch(`${URL}/data`).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    //console.log(res);
+    throw new Error(res.statusText);
+  });
 };
 
+readData().then(res => {
+  data = res[0];
+  // console.log(data);
+ // console.log(data.array);
+});
+//Записує данні на сайт
+const updateData = (id, dataPut) => {
+  return fetch(`${URL}/data/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(dataPut),
+  });
+};
 
-
-refs.buttonPlay.addEventListener("click", playSound);
+refs.buttonPlay.addEventListener('click', playSound);
+refs.buttonAdd.addEventListener('click', addToBase);
+ //додає слова в базу 
+function addToBase() {
+  let translatePair = [
+    refs.engWord.value.toLowerCase(),
+    refs.ruWord.value.toLowerCase()
+  ];
+  if (data !== '' && refs.engWord.value !== '' && refs.ruWord.value !== "") {
+   // console.log(data);
+    data.array.push(translatePair);
+    console.log(data);
+    updateData(1, data);
+  }
+  console.log(translatePair);
+}
 
 function playSound() {
-  word = refs.name.value.trim().toLowerCase();
+  word = refs.engWord.value.trim().toLowerCase();
   if (word !== '') {
     try {
       console.log('Play sound');
@@ -38,90 +80,81 @@ function playSound() {
 }
 
 // Додає слухача та використовує функцію debounce, яка робить HTTP-запит через 300мс після того, як користувач перестав вводити текст
-refs.name.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+refs.engWord.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(event) {
-    // записуємо в змінну введене значення користувачем (trim прибирає пробіли)
-    let inputEl = refs.name.value.trim();
- // word = inputEl;
-// очистка списку країн та інформації про країну
-    refs.countriesInfo.innerHTML = "";
-    refs.countriesList.innerHTML = "";
+  // записуємо в змінну введене значення користувачем (trim прибирає пробіли)
+  let input = refs.engWord.value.trim();
+  // очистка списку країн та інформації про країну
+  refs.countriesInfo.innerHTML = '';
+  refs.countriesList.innerHTML = '';
 
-// перевірка, якщо значення не пустий рядок
-    
-  if (inputEl !== "") {
-    const sl = "en";
-    const tl = "ru";
-  
-  let translateUrl = `https://translate.googleapis.com/translate_a/single?format=text&client=gtx&sl=${sl}&tl=${tl}&dt=t&q=${inputEl}`;
-  // sl – язык оригинала, tl – язык для перевода, originalText – текст запроса (можно использовать результат string.match(/.{1,2000}(?=\.)/gi))
-  axios
-    .get(translateUrl)
-    .then(function (response) {
-      translatedText = response.data[0][0][0];
-      
-      console.log(translatedText);
-      refs.ruWord.value = translatedText;
-      let translatePair = [inputEl, refs.ruWord.value]; ; 
-      console.log(translatePair);
-playSound();
+  // перевірка, якщо значення не пустий рядок
 
-      // do something with the translated text
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  if (input !== '') {
+    const sl = 'en';
+    const tl = 'ru';
+
+    let translateUrl = `https://translate.googleapis.com/translate_a/single?format=text&client=gtx&sl=${sl}&tl=${tl}&dt=t&q=${input}`;
+    // sl – язык оригинала, tl – язык для перевода, originalText – текст запроса (можно использовать результат string.match(/.{1,2000}(?=\.)/gi))
+    axios
+      .get(translateUrl)
+      .then(function (response) {
+        translatedText = response.data[0][0][0];
+
+        console.log(translatedText);
+        refs.ruWord.value = translatedText;
+
+        playSound();
+
+        // do something with the translated text
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  // якщо користувач ввів не иснуючу назву країни, то відображає повідомлення
+  if (1 === 0) {
+    Notiflix.Notify.failure(`Oops, there is no country with that name`);
+  }
 }
-
-        // якщо користувач ввів не иснуючу назву країни, то відображає повідомлення
-      if (1=== 0) {
-        Notiflix.Notify.failure(`Oops, there is no country with that name`);
-      }
-    
-  
- };
-    // Розмітка для однієї країни
-    const createListCountries =
-      item => `<li class="list list__info"><img src="${item.flags.png}" alt="${item.flags.alt}" width="30" height="20"/>
+// Розмітка для однієї країни
+const createListCountries =
+  item => `<li class="list list__info"><img src="${item.flags.png}" alt="${item.flags.alt}" width="30" height="20"/>
   <p class = "text">  ${item.name.official}</p></li>`;
-        
-   
 
 // Перебір масива
-const generateContent = (array) => array.reduce((acc, item) => {
+const generateContent = array =>
+  array.reduce((acc, item) => {
     return acc + createListCountries(item);
-}, "");
-
+  }, '');
 
 // Додавання в DOM
-const insertContent = (array) => {
-    const result = generateContent(array);
-    refs.countriesList.insertAdjacentHTML('beforeend', result);
+const insertContent = array => {
+  const result = generateContent(array);
+  refs.countriesList.insertAdjacentHTML('beforeend', result);
 };
-
 
 // Розмітка з інформацією для однієї країни (прапор, назва, столиця, населення, мови)
 const createInfoCountries = item =>
-    `<li class="list"><img src="${
-  item.flags.png
-}" alt="${item.flags.alt}" width="60" hight="40"/>
+  `<li class="list"><img src="${item.flags.png}" alt="${
+    item.flags.alt
+  }" width="60" hight="40"/>
   <p class="name"><b>${item.name.official}</b></p>
   <p><b>Capital</b>: ${item.capital}</p>
    <p><b>Population</b>: ${item.population}</p>
-   <p><b>Languages</b>: ${Object.values(item.languages).join(", ")}</p></li>
+   <p><b>Languages</b>: ${Object.values(item.languages).join(', ')}</p></li>
   `;
 
-
-  // Перебір масива
-const generateContentInfo = (array) => array.reduce((acc, item) => {
-   
+// Перебір масива
+const generateContentInfo = array =>
+  array.reduce((acc, item) => {
     return acc + createInfoCountries(item);
-}, "");
-
+  }, '');
 
 // Додавання в DOM
-const insertContentInfo = (array) => {
-    const result = generateContentInfo(array);
-    refs.countriesInfo.insertAdjacentHTML('beforeend', result);
+const insertContentInfo = array => {
+  const result = generateContentInfo(array);
+  refs.countriesInfo.insertAdjacentHTML('beforeend', result);
 };
